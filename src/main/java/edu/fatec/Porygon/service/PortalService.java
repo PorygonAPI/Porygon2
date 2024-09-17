690a565ab3cebc1efa76d4939f3b8bfa72b94346
João Pedro <marcondesjpb@gmail.com>
2024-09-17 16:49:46 -0300

    PYG-7:Build: Adicionado Labels do Web Scraping no Front e Salvamento automático da data.
    PYG-30:Refactor: Adicionado colunas do Web Scraping na Tabela Portal.

--- src/main/java/edu/fatec/Porygon/service/PortalService.java
+++ src/main/java/edu/fatec/Porygon/service/PortalService.java
@@ -5,6 +5,7 @@ import edu.fatec.Porygon.repository.PortalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,6 +24,7 @@ public class PortalService {
     }
 
     public Portal salvar(Portal portal) {
+        portal.setDataSave(LocalDate.now());
         return portalRepository.save(portal);
     }
 
