74b0585c947a88e040a7fb060285e4a9486e696c
RenatoCMMendes <renato.mendes2@fatec.sp.gov.br>
2024-09-19 14:35:58 -0300

    /PYG-2-PYG-28-CRUD-e-front-Criar-a-op-o-deletar-desativar-dados-dos-portais-cadastrados

--- src/main/java/edu/fatec/Porygon/controller/PortalController.java
+++ src/main/java/edu/fatec/Porygon/controller/PortalController.java
@@ -4,16 +4,15 @@ import edu.fatec.Porygon.model.Portal;
 import edu.fatec.Porygon.repository.AgendadorRepository;
 import edu.fatec.Porygon.repository.PortalRepository;
 import edu.fatec.Porygon.repository.TagRepository;
+import edu.fatec.Porygon.service.PortalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
 import java.time.LocalDate;
-import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/portais")
@@ -28,6 +27,9 @@ public class PortalController {
     @Autowired
     private TagRepository tagRepository;
 
+    @Autowired
+    private PortalService portalService;  // Injetando o PortalService para acessar os métodos de serviço
+
     @GetMapping()
     public String mostrarFormularioCadastro(Model model) {
         model.addAttribute("portal", new Portal());
@@ -36,9 +38,6 @@ public class PortalController {
         model.addAttribute("tags", tagRepository.findAll());
         return "portal";
     }
-
-
-
 
     @GetMapping("/editar/{id}")
     public String mostrarFormularioEdicao(@PathVariable("id") Integer id, Model model) {
@@ -61,13 +60,21 @@ public class PortalController {
         return "redirect:/portais";
     }
 
+    // Método para alterar o status (ativo/desativado) de um portal
+    @PostMapping("/alterarStatus/{id}")
+    public ResponseEntity<?> alterarStatus(@PathVariable Integer id, @RequestBody Map<String, Boolean> body) {
+        boolean novoStatus = body.get("ativo");
+        portalService.alterarStatus(id, novoStatus);  // Chama o serviço para alterar o status
+        return ResponseEntity.ok().build();
+    }
+
     // @GetMapping("/alternar/{id}")
     // public String alternarAtivo(@PathVariable("id") Integer id) {
     //     Portal portal = portalRepository.findById(id)
     //         .orElseThrow(() -> new IllegalArgumentException("ID inválido: " + id));
 
-    //     portal.setAtivo(!portal.isAtivo()); 
+    //     portal.setAtivo(!portal.isAtivo());
     //     portalRepository.save(portal);
-    //     return "redirect:/portais"; 
+    //     return "redirect:/portais";
     // }
 }
