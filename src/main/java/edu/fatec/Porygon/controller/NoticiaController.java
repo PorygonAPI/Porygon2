2ba3953da589967714d59a42ba3f42b569a3339a
lucasalex1203 <lucasalex1203@gmail.com>
2024-09-20 22:40:24 -0300

    PY2-PY8-(fix): correção de listagem

--- src/main/java/edu/fatec/Porygon/controller/NoticiaController.java
+++ src/main/java/edu/fatec/Porygon/controller/NoticiaController.java
@@ -1,22 +1,28 @@
 package edu.fatec.Porygon.controller;
 
+import edu.fatec.Porygon.model.Noticia;
 import edu.fatec.Porygon.repository.NoticiaRepository;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class NoticiaController { //Lucas
+public class NoticiaController {
 
     @Autowired
-    private NoticiaRepository NoticiaRepository;
+    private NoticiaRepository noticiaRepository; // camelCase
 
-    @GetMapping("/noticias") //endpoint - 
-    public String listarNoticias (Model model) {
-        model.addAttribute("noticias", NoticiaRepository.findAll());
+    @GetMapping("/index")
+    public String listarNoticias(Model model) {
+        List<Noticia> noticias = noticiaRepository.findAll();
+        model.addAttribute("noticias", noticias);
         return "index";
     }
 
-    
 }
+
+
