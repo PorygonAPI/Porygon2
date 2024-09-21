1fd8fb7be3cae7fcf430a43bb2b4e3b03a89dc35
Vitor F. Morais <111707785+vmorais111@users.noreply.github.com>
2024-09-20 22:04:04 -0300

    atualização branch

--- src/main/java/edu/fatec/Porygon/service/DataScrapper.java
+++ src/main/java/edu/fatec/Porygon/service/DataScrapper.java
@@ -1,5 +1,6 @@
 package edu.fatec.Porygon.service;
 
+import edu.fatec.Porygon.model.Noticia;
 import edu.fatec.Porygon.repository.NoticiaRepository;
 import edu.fatec.Porygon.repository.PortalRepository;
 import org.jsoup.Jsoup;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.List;
 
 @Service
 public class DataScrapper {
@@ -20,28 +22,31 @@ public class DataScrapper {
     private PortalRepository portalRepository;
 
 
+    public List<Noticia> teste(){ return noticiaRepository.findAll(); };
 
 
-//    public void scrapeData(String url){
-//       try{
-//           //Criando a conexão como site por meio do Jsoup
-//           Document doc = Jsoup.connect(url).get();
-//
-//           //Criando a estrutura para iterar a página
-//
-//
-//           //Criando a estrutura para salvar os elementos da notícia
-//           Element Titulo, Data, Autor;
-//           Elements conteudo;
-//
-//
-//           //Salvar os arquivos no banco de dados
-//
-//
-//       } catch (IOException e) {
-//           throw new RuntimeException(e);
-//       }
-//
-//    }
+
+    public void scrapeData(String url){
+
+       try{
+           //Criando a conexão como site por meio do Jsoup
+           Document doc = Jsoup.connect(url).get();
+
+           //Criando a estrutura para iterar a página
+
+
+           //Criando a estrutura para salvar os elementos da notícia
+           Element Titulo, Data, Autor;
+           Elements conteudo;
+
+
+           //Salvar os arquivos no banco de dados
+
+
+       } catch (IOException e) {
+           throw new RuntimeException(e);
+       }
+
+    }
 
 }
