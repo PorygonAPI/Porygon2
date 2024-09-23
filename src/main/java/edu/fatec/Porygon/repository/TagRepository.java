03661ffc81baf4996b709f070653a5abccf7431c
Pablo <pablo.silva61@fatec.sp.gov.br>
2024-09-23 17:53:26 -0300

    PYG-9 PYG-13 fix: deletando arquivos para evitar conflito

--- src/main/java/edu/fatec/Porygon/repository/TagRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.fatec.Porygon.repository;
-
-import edu.fatec.Porygon.model.Tag;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TagRepository extends JpaRepository<Tag, Integer> {
-}
