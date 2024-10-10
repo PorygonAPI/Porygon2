167f912a5f570c9524fede2dc56fc40151d11687
Ruth mira <143049504+RuthMira@users.noreply.github.com>
2024-10-10 13:01:37 +0000

    refactor/frontend trazendo selected no campo do formatador

--- src/main/java/edu/fatec/Porygon/PorygonApplication.java
+++ src/main/java/edu/fatec/Porygon/PorygonApplication.java
@@ -1,6 +1,5 @@
 package edu.fatec.Porygon;
 
-import edu.fatec.Porygon.service.DataScrapperService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
