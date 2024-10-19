8a514868a945e022b2b1a451607d466e47cdd30c
lucasalex1203 <lucasalex1203@gmail.com>
2024-10-18 22:41:25 -0300

    PYG-17 PYG-45 feat/fix paginacao e resolvendo conflitos
    Co-Authored-By: Pablo <143049920+pablohgs05@users.noreply.github.com>

--- src/main/java/edu/fatec/Porygon/controller/NoticiaController.java
+++ src/main/java/edu/fatec/Porygon/controller/NoticiaController.java
@@ -18,6 +18,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 
 @Controller
@@ -29,19 +32,45 @@ public class NoticiaController {
     private NoticiaService noticiaService;
 
     // private static final Logger logger = (Logger) LoggerFactory.getLogger(NoticiaController.class);
+    @GetMapping("/")
+    public String redirecionarParaIndex() {
+    return "redirect:/index";
+    }
 
     @GetMapping("/index")
-    public String listarNoticias(Model model) {
-        List<Noticia> noticias = noticiaRepository.findAll();
-        model.addAttribute("noticias", noticias);
-        return "index";
+    public String listarNoticias(@RequestParam(defaultValue = "0") int page, Model model) {
+    int pageSize = 5; // Tamanho da página
+    Page<Noticia> noticiasPage = noticiaRepository.findAll(PageRequest.of(page, pageSize));
+
+    model.addAttribute("noticias", noticiasPage.getContent());
+    model.addAttribute("paginaAtual", page + 1); // Para mostrar a página de forma amigável (1-indexed)
+    model.addAttribute("paginaAnterior", page - 1); // Para mostrar a página de forma amigável (1-indexed)
+    model.addAttribute("totalPaginas", noticiasPage.getTotalPages());
+
+    return "index";
     }
+
+    // Método para buscar o detalhe de uma notícia por ID
     @GetMapping("/noticias/detalhe/{id}")
     @ResponseBody
     public ResponseEntity<Noticia> detalheNoticiaJson(@PathVariable Integer id) {
-        Optional<Noticia> noticiaOptional = noticiaRepository.findById(id);
-        return noticiaOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        Optional<Noticia> noticiaOptional = noticiaService.findById(id);
+        return noticiaOptional.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
+
+    // @GetMapping("/index")
+    // public String listarNoticias(Model model) {
+    //     List<Noticia> noticias = noticiaRepository.findAll();
+    //     model.addAttribute("noticias", noticias);
+    //     return "index";
+    // }
+    // @GetMapping("/noticias/detalhe/{id}")
+    // @ResponseBody
+    // public ResponseEntity<Noticia> detalheNoticiaJson(@PathVariable Integer id) {
+    //     Optional<Noticia> noticiaOptional = noticiaRepository.findById(id);
+    //     return noticiaOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    // }
 
      @GetMapping("/noticias")
      @ResponseBody
--- src/main/java/edu/fatec/Porygon/service/NoticiaService.java
+++ src/main/java/edu/fatec/Porygon/service/NoticiaService.java
@@ -2,7 +2,11 @@ package edu.fatec.Porygon.service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import edu.fatec.Porygon.model.Noticia;
 import edu.fatec.Porygon.repository.NoticiaRepository;
@@ -19,5 +23,13 @@ public class NoticiaService {
 
     public List<Noticia> listarNoticiasPorData(LocalDate dataInicio, LocalDate dataFim) {
          return noticiaRepository.searchNewsByData(dataInicio, dataFim);
+    }
+        
+    public Page<Noticia> listarNoticias(Pageable pageable) {
+        return noticiaRepository.findAll(pageable);
+    }
+
+    public Optional<Noticia> findById(Integer id) {
+        return noticiaRepository.findById(id); 
     }
 }
