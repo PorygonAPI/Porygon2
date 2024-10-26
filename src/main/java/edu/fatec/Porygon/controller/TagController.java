53dd6dbaddc038a703c6b847b1acae08c7db0a12
João Pedro <marcondesjpb@gmail.com>
2024-10-26 17:48:58 -0300

    PYG-14/PYG-68/Feat(controller)/Verificacao-Regionalismo

--- src/main/java/edu/fatec/Porygon/controller/TagController.java
+++ src/main/java/edu/fatec/Porygon/controller/TagController.java
@@ -5,9 +5,7 @@ import edu.fatec.Porygon.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -29,6 +27,17 @@ public class TagController {
         try {
             tagService.criarTag(tag);
             redirectAttributes.addFlashAttribute("mensagemSucesso", "Tag cadastrada com sucesso!");
+        } catch (RuntimeException e) {
+            redirectAttributes.addFlashAttribute("mensagemErro", e.getMessage());
+        }
+        return "redirect:/tags";
+    }
+
+    @PostMapping("/editar/{id}")
+    public String editarTag(@PathVariable Integer id, @RequestParam String novoNome, RedirectAttributes redirectAttributes) {
+        try {
+            tagService.editarTag(id, novoNome);
+            redirectAttributes.addFlashAttribute("mensagemSucesso", "Tag atualizada com sucesso!");
         } catch (RuntimeException e) {
             redirectAttributes.addFlashAttribute("mensagemErro", e.getMessage());
         }
