d9ad2ec09ebef001660e89e13a1bd19028e6cc9a
Vitor F. Morais <111707785+vmorais111@users.noreply.github.com>
2024-11-06 23:55:17 -0300

    pyg-110/PYG-114/feat(NoticiaService):função-para-buscar-sinonimos-no-titulo

--- src/main/java/edu/fatec/Porygon/service/NoticiaService.java
+++ src/main/java/edu/fatec/Porygon/service/NoticiaService.java
@@ -1,7 +1,15 @@
 package edu.fatec.Porygon.service;
 
+import java.text.Normalizer;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+
+import edu.fatec.Porygon.model.Sinonimo;
+import edu.fatec.Porygon.model.Tag;
+import edu.fatec.Porygon.repository.SinonimoRepository;
+import edu.fatec.Porygon.repository.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import edu.fatec.Porygon.model.Noticia;
@@ -13,11 +21,61 @@ public class NoticiaService {
     @Autowired
     private NoticiaRepository noticiaRepository;
 
+    @Autowired
+    private TagRepository tagRepository;
+    @Autowired
+    private SinonimoRepository sinonimoRepository;
+
     public List<Noticia> listarNoticias() {
         return noticiaRepository.findAll();
     }
 
     public List<Noticia> listarNoticiasPorData(LocalDate dataInicio, LocalDate dataFim) {
          return noticiaRepository.searchNewsByData(dataInicio, dataFim);
+    }
+
+    public void findTagsInTitle() {
+        List<Tag> tags = tagRepository.findAll();
+        List<Noticia> news = noticiaRepository.findAll();
+
+        for (Noticia noticia : news) {
+            HashSet<Tag> foundTags = new HashSet<>();
+            for (Tag tag : tags) {
+                if(noticia.getTitulo().toLowerCase().contains(tag.getNome().toLowerCase())){
+                    foundTags.add(tag);
+                }else{
+                    Tag tagFound = searchSynonymsInTitle(tag, noticia);
+                    if (tagFound != null) {
+                        foundTags.add(tagFound);
+                    }
+                }
+            }
+
+            if(!foundTags.isEmpty()) {
+                noticia.setTags(new ArrayList<>(foundTags));
+                noticiaRepository.save(noticia);
+            }
+        }
+    }
+
+    public Tag searchSynonymsInTitle(Tag tag, Noticia noticia) {
+        List<Sinonimo> sinonimos = sinonimoRepository.findByTag(tag);
+        Tag retorno = null;
+
+        String cleanTitleAccent = Normalizer.normalize(noticia.getTitulo(), Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}", "");
+        String cleanTitleSpecialCharacter = cleanTitleAccent.replaceAll("[^a-zA-Z0-9\\s]", "").toLowerCase();
+        String[] words = cleanTitleSpecialCharacter.split("\\s+");
+
+        outerLoop:
+        for (String word : words) {
+            for(Sinonimo sinonimo : sinonimos) {
+                if (sinonimo.getNome().toLowerCase().equals(word)){
+                    retorno=tag;
+                    break outerLoop;
+                }
+            }
+        }
+
+        return retorno;
     }
 }
