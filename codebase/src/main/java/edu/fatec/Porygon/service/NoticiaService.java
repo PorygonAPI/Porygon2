5c82f0d9e8cf12f3d72433b72e8db4909ff6f2c2
Ruth mira <143049504+RuthMira@users.noreply.github.com>
2024-11-11 20:02:31 +0000

    PYG-122 PYG-118 feat: refatora os métodos de associar tags para otimizar

--- codebase/src/main/java/edu/fatec/Porygon/service/NoticiaService.java
+++ codebase/src/main/java/edu/fatec/Porygon/service/NoticiaService.java
@@ -8,7 +8,6 @@ import edu.fatec.Porygon.model.Sinonimo;
 import edu.fatec.Porygon.model.Tag;
 import edu.fatec.Porygon.repository.SinonimoRepository;
 import edu.fatec.Porygon.repository.TagRepository;
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import edu.fatec.Porygon.model.Noticia;
@@ -34,155 +33,79 @@ public class NoticiaService {
         return noticiaRepository.searchNewsByData(dataInicio, dataFim);
     }
 
-    public Noticia salvar(Noticia noticia, List<Integer> tagIds) {
-        if (tagIds != null && !tagIds.isEmpty()) {
-            Set<Tag> tagsPortal = new HashSet<>(tagRepository.findAllById(tagIds));
-            associarTagsRelevantes(noticia, tagsPortal);
-        }
-        return noticiaRepository.save(noticia);
+    public Noticia salvar(Noticia noticia) {
+        Noticia savedNoticia = noticiaRepository.save(noticia);
+        associarTags(savedNoticia);
+
+        return savedNoticia;
     }
 
-    public Noticia atualizarTags(Integer noticiaId, List<Integer> tagIds) {
-        Noticia noticia = noticiaRepository.findById(noticiaId)
-                .orElseThrow(() -> new RuntimeException("Notícia não encontrada"));
+    private void associarTags(Noticia noticia) {
+        Set<Tag> tagsPortal = new HashSet<>(tagRepository.findAllByPortais_Id(noticia.getPortal().getId())); 
 
-        if (tagIds != null) {
-            Set<Tag> tagsPortal = tagIds.isEmpty()
-                    ? new HashSet<>()
-                    : new HashSet<>(tagRepository.findAllById(tagIds));
-            associarTagsRelevantes(noticia, tagsPortal);
+        Set<Tag> foundTags = encontrarTagsNoTitulo(noticia.getTitulo(), tagsPortal);
+        if (foundTags.isEmpty()) {
+            foundTags = encontrarTagsNoConteudo(noticia.getConteudo(), tagsPortal);
+        }
+        if (foundTags.isEmpty()) {
+            foundTags = encontrarSinonimos(noticia.getTitulo(), noticia.getConteudo(), tagsPortal);
         }
 
-        return noticiaRepository.save(noticia);
+        noticia.setTags(foundTags);
+        noticiaRepository.save(noticia);
     }
 
-    @Transactional
-    public void findTagsInTitle() {
-        List<Tag> tags = tagRepository.findAll();
-        List<Noticia> news = noticiaRepository.findAll();
+    private Set<Tag> encontrarTagsNoTitulo(String titulo, Set<Tag> tagsPortal) {
+        Set<Tag> foundTags = new HashSet<>();
+        String tituloNormalizado = normalizarTexto(titulo); 
+        for (Tag tag : tagsPortal) {
+            String tagNormalizada = normalizarTexto(tag.getNome()); 
+            if (tituloNormalizado.contains(tagNormalizada)) {
+                foundTags.add(tag);
+            }
+        }
+        return foundTags;
+    }
 
-        for (Noticia noticia : news) {
-            HashSet<Tag> foundTags = new HashSet<>();
-            for (Tag tag : tags) {
-                if (noticia.getTitulo().toLowerCase().contains(tag.getNome().toLowerCase())) {
+    private Set<Tag> encontrarTagsNoConteudo(String conteudo, Set<Tag> tagsPortal) {
+        Set<Tag> foundTags = new HashSet<>();
+        String conteudoNormalizado = normalizarTexto(conteudo);
+        for (Tag tag : tagsPortal) {
+            String tagNormalizada = normalizarTexto(tag.getNome()); 
+            if (conteudoNormalizado.contains(tagNormalizada)) {
+                foundTags.add(tag);
+            }
+        }
+        return foundTags;
+    }
+
+    private Set<Tag> encontrarSinonimos(String titulo, String conteudo, Set<Tag> tagsPortal) {
+        Set<Tag> foundTags = new HashSet<>();
+        String tituloNormalizado = normalizarTexto(titulo);
+        String conteudoNormalizado = normalizarTexto(conteudo); 
+        for (Tag tag : tagsPortal) {
+            List<Sinonimo> sinonimos = sinonimoRepository.findByTag(tag);
+            for (Sinonimo sinonimo : sinonimos) {
+                String sinonimoNormalizado = normalizarTexto(sinonimo.getNome()); 
+                if (tituloNormalizado.contains(sinonimoNormalizado) ||
+                        conteudoNormalizado.contains(sinonimoNormalizado)) {
                     foundTags.add(tag);
-                } else {
-                    Tag tagFound = searchSynonymsInTitle(tag, noticia);
-                    if (tagFound != null) {
-                        foundTags.add(tagFound);
-                    }
-                }
-            }
-
-            if (!foundTags.isEmpty()) {
-                noticia.setTags(foundTags);
-                noticiaRepository.save(noticia);
-            }
-
-            associarTagsPorConteudo();
-        }
-    }
-
-
-    public Tag searchSynonymsInTitle(Tag tag, Noticia noticia) {
-        List<Sinonimo> sinonimos = sinonimoRepository.findByTag(tag);
-        Tag retorno = null;
-
-        String cleanTitleAccent = Normalizer.normalize(noticia.getTitulo(), Normalizer.Form.NFD).replaceAll("\\p{InCombiningDiacriticalMarks}", "");
-        String cleanTitleSpecialCharacter = cleanTitleAccent.replaceAll("[^a-zA-Z0-9\\s]", "").toLowerCase();
-        String[] words = cleanTitleSpecialCharacter.split("\\s+");
-
-        for (String word : words) {
-            for(Sinonimo sinonimo : sinonimos) {
-                if (sinonimo.getNome().toLowerCase().equals(word)){
-                    return tag;
-                }
-            }
-        }
-
-        return null;
-    }
-
-    @Transactional
-    public void associarTagsPorConteudo() {
-        List<Tag> tags = tagRepository.findAll();
-        List<Noticia> noticias = noticiaRepository.findAll();
-
-        for (Noticia noticia : noticias) {
-            for (Tag tag : tags) {
-                if (buscarSinonimoNoConteudo(tag, noticia)) {
-                    if (noticia.getTags() == null) {
-                        noticia.setTags(new HashSet<>()); 
-                    }
-
-                    if (!noticia.getTags().contains(tag)) {
-                        noticia.getTags().add(tag);
-                        noticiaRepository.save(noticia);
-                    }
-
                     break;
                 }
             }
         }
-    }
-
-    private boolean buscarSinonimoNoConteudo(Tag tag, Noticia noticia) {
-        List<Sinonimo> sinonimos = sinonimoRepository.findByTag(tag);
-
-        String conteudoLimpo = Normalizer.normalize(noticia.getConteudo(), Normalizer.Form.NFD)
-                .replaceAll("\\p{InCombiningDiacriticalMarks}", "")
-                .replaceAll("[^a-zA-Z0-9\\s]", "").toLowerCase();
-
-        String[] palavrasConteudo = conteudoLimpo.split("\\s+");
-
-        for (String palavra : palavrasConteudo) {
-            for (Sinonimo sinonimo : sinonimos) {
-                if (sinonimo.getNome().toLowerCase().equals(palavra)) {
-                    return true; 
-                }
-            }
-        }
-        return false; 
-    }
-
-    private void associarTagsRelevantes(Noticia noticia, Set<Tag> tagsPortal) {
-        Set<Tag> tagsRelevantes = new HashSet<>();
-
-        String conteudoNormalizado = normalizarTexto(noticia.getConteudo());
-        String tituloNormalizado = normalizarTexto(noticia.getTitulo());
-
-        for (Tag tag : tagsPortal) {
-            String tagNormalizada = normalizarTexto(tag.getNome());
-            String[] palavrasTag = tagNormalizada.split("[-\\s]");
-            boolean tagEncontrada;
-
-            if (palavrasTag.length > 1) {
-                tagEncontrada = Arrays.stream(palavrasTag)
-                        .allMatch(palavra -> conteudoNormalizado.contains(palavra) ||
-                                tituloNormalizado.contains(palavra));
-            } else {
-                tagEncontrada = conteudoNormalizado.contains(tagNormalizada) ||
-                        tituloNormalizado.contains(tagNormalizada);
-            }
-
-            if (tagEncontrada) {
-                tagsRelevantes.add(tag);
-            }
-        }
-
-        noticia.setTags(tagsRelevantes);
+        return foundTags;
     }
 
     private String normalizarTexto(String texto) {
-        if (texto == null)
-            return "";
-        return texto.toLowerCase()
-                .replaceAll("[áàãâä]", "a")
-                .replaceAll("[éèêë]", "e")
-                .replaceAll("[íìîï]", "i")
-                .replaceAll("[óòõôö]", "o")
-                .replaceAll("[úùûü]", "u")
-                .replaceAll("[ç]", "c")
-                .replaceAll("[^a-z0-9\\s-]", "");
+        if (texto == null) {
+            return null;
+        }
+
+        texto = texto.toLowerCase();
+        texto = Normalizer.normalize(texto, Normalizer.Form.NFD);
+        texto = texto.replaceAll("[^\\p{ASCII}]", "");
+
+        return texto;
     }
 }
